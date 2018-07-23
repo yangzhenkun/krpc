4a40f633847a2d0873af998fbcde1dad4a06cf71
yangzhenkun <1334036616@qq.com>
2018-07-23 18:50:36 +0800

    tt

--- com.krpc.client/src/main/java/com/krpc/client/core/LoadBalance.java
+++ com.krpc.client/src/main/java/com/krpc/client/core/LoadBalance.java
@@ -18,6 +18,7 @@ public class LoadBalance {
 	 * @return
 	 */
 	public static Address loadbalance(String serviceName){
+		
 		ServiceParams serviceParams = KRPC.getService(serviceName);
 		int total = serviceParams.getAddresses().size();
 		int index = (int) (System.currentTimeMillis()%total);
